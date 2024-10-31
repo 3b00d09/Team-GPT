74b6fc445d0518382492f76f0cac26fde70e0b9c
3b00d09 <bd315977@gmail.com>
2024-10-31 01:08:33 +0000

    testing if config was the problem

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -5,12 +5,7 @@ import * as schema from "./schema"
 const client = createClient({
   url: process.env.DB_URL!,
   authToken: process.env.DB_KEY!,
-  fetch: (url: string, options: any) => {
-    return fetch(url, {
-      ...options,
-      timeout: 30000, 
-    });
-  },
+  
 });
 
 export const dbClient = drizzle(client, { schema: schema});
